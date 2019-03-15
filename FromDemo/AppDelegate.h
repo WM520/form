4425ffb1b18834c4cec27060465d6267fba05778
鼠非鱼 <n_sfyu@3songshu.com>
2019-03-15 10:09:50 +0800

    Initial Commit

---
+++ FromDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FromDemo
+//
+//  Created by miao on 2019/3/15.
+//  Copyright © 2019 miao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
